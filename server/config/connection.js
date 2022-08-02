b5b3d0bd77ae30a649e320c38fa1ef720f2b2c1b
smckelli <smckelli@gmail.com>
2022-08-02 16:48:13 -0500

    cleaned up some serverside

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/example_db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/ToDo', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
