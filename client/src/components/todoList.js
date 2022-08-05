1b8f76422aa14f5a85b7d0e92707ca11f43e7f7b
smckelli <smckelli@gmail.com>
2022-08-05 08:45:59 -0500

    got query posted to show in list 1x

--- client/src/components/todoList.js
+++ client/src/components/todoList.js
@@ -1,34 +1,37 @@
 // useQuery use the useQuery hook
 // read ALL_TODOS from database and use for map
 import React from 'react';
+import { useParams } from 'react-router-dom'
 import {useQuery } from '@apollo/client'
 import { ALL_TODOS } from '../utils/queries';
 
 
-
 const TodoList = ({ todos }) => {
-
-  const { loading, error, data } = useQuery(ALL_TODOS); {
-    variables: { todos }
-  };
+  const { id } = useParams()
+  const { loading, error, data } = useQuery(ALL_TODOS, {
+    variables: {
+      _id: id
+    }
+  });
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
-  if (!todos.length) {
+  console.log(data)
+
+  if (!data.todos.length) {
     return <h3>No Tasks Yet!</h3>;
   }
   return (
     <>
       <h2 className="text-secondary">Todo List:</h2>
       <ul className="list-group">
-        {todos.map(todo => {
+        {data.todos.map((todo, i) => {
           const completeClass = todo?.complete ? 'text-decoration-line-through text-primary' : ''
           return (
             <li 
               className="list-group-item d-flex justify-content-between bg-dark text-light fs-4" 
-              key={todo._id}
-            >
+              key={`${todo._id}-${i}`}>
               <div>
                 <input
                   className="form-check-input me-3" 
