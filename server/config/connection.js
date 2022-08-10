a79b63c15dc00e80ac79c8bc2880440a83478c5c
smckelli <smckelli@gmail.com>
2022-08-09 22:34:23 -0500

    heroku won't start

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/ToDo', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ToDo', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
